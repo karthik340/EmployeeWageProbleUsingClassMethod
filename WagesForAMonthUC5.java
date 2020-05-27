b569b9a16543e1ce0cf659ede6782cc940ac9447
karthik <nallaboluk@gmail.com>
2020-05-27 15:48:13 +0530

    adding WagesForAMonthUC5

---
+++ WagesForAMonthUC5.java
@@ -0,0 +1,40 @@
+import java.util.*; 
+public class WagesForAMonthUC5 {
+		public static final int isFullTime = 1;
+		public static final int isPartTime = 2;
+	
+		public static int findSalary()
+		{
+		int wagePerHour = 20;
+		int empHrs;
+		int salary;
+		int totalSalary=0;
+		int numWorkingDays=20;
+		int empCheck;
+		for(int day=1;day<=numWorkingDays;day++)
+		{
+			empCheck =(int) Math.floor(Math.random() * 10)%3;         
+			switch(empCheck)
+		        {
+		        	case isFullTime:
+					empHrs=8;
+					break;
+				case isPartTime:
+					empHrs=4;
+					break;
+				default:
+					empHrs=0;
+					break;
+		        }
+			salary = wagePerHour * empHrs;
+			totalSalary+=salary;
+		}
+		return totalSalary;
+		}
+		public static void main(String[] args) {
+		
+			System.out.println("salary"+findSalary());
+		}
+}
+
+
